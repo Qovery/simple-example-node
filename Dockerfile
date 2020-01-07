44e366caf29162712181d76887457b64fd30437c
Romaric Philogene <evoxmusic@gmail.com>
2020-01-07 17:51:40 +0100

    add: qovery dependency

--- Dockerfile
+++ Dockerfile
@@ -10,4 +10,4 @@ RUN npm install
 
 EXPOSE 3000
 
-CMD ["npm", "run", "start"]
+RUN npm run start
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const Qovery = require('qovery-client/src/Qovery');
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index', {title: 'Express'});
+    res.render('index', {title: 'Express', branchName: new Qovery().getBranchName()});
 });
 
 module.exports = router;
